a335c6f322871299efdaa499688e00e055b6804c
Anuj Sharma <anujsharmaiitbombay2024@gmail.com>
2024-12-09 11:14:03 +0530

    User model creation

---
+++ Server/Model/Users.js
@@ -0,0 +1,42 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../Database/db";
+const User = sequelize.define(
+  "User",
+  {
+    // Email column as primary key
+    email: {
+      type: DataTypes.STRING, // Email is stored as a string
+      allowNull: false,
+      // Email cannot be null
+    },
+    // Password column
+    password: {
+      type: DataTypes.STRING, // Password is stored as a string (hashed)
+      allowNull: false, // Password cannot be null
+    },
+    display_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    user_blocked: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+  },
+  {
+    // Model options
+
+    timestamps: true, // Disable automatic creation of createdAt and updatedAt
+    tableName: "users", // Specify the table name explicitly
+  }
+);
+
+// Export the model
+export default User;
