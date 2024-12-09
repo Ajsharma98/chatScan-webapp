2a7d9ec231c79a4bb2f9334864c38e6389a3aa70
Anuj Sharma <anujsharmaiitbombay2024@gmail.com>
2024-12-09 11:59:45 +0530

    Participant model creation with room and user id forgein key

--- Server/Model/participant.js
+++ Server/Model/participant.js
@@ -1,5 +1,7 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../Database/db";
+import { Room } from "./Room";
+import { User } from "./User";
 const Participant = sequelize.define(
   "Participant",
   {
@@ -14,10 +16,18 @@ const Participant = sequelize.define(
     room_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      references: {
+        model: "Room",
+        key: "room_id",
+      },
     },
     user_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      references: {
+        model: "User",
+        key: "user_id",
+      },
     },
   },
   {
